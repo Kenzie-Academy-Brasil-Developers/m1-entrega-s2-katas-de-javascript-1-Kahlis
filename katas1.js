ca85dbf5447051dfc938ee40c5517c8c293ab085
Matheus de Souza Arruda <kahlis.dev@gmail.com>
2022-02-18 16:18:52 -0300

    Katas1 updated

--- katas1.js
+++ katas1.js
@@ -6,6 +6,7 @@ function oneThroughTwenty(){
     }
     return result;
 }
+console.log(oneThroughTwenty());
 
 //Retornar os números pares de 1 a 20. (2, 4, 6,…, 18, 20)
 function evensToTwenty(){
@@ -17,6 +18,7 @@ function evensToTwenty(){
     }
     return result;
 }
+console.log(evensToTwenty());
 
 //Retornar os números ímpares de 1 a 20. (1, 3, 5,…, 17, 19)
 function oddsToTwenty(){
@@ -28,6 +30,7 @@ function oddsToTwenty(){
     }
     return result;
 }
+console.log(oddsToTwenty());
 
 //Retornar os múltiplos de 5 até 100. (5, 10, 15,…, 95, 100)
 function multiplesOfFive(){
@@ -39,6 +42,7 @@ function multiplesOfFive(){
     }
     return result;
 }
+console.log(multiplesOfFive());
 
 //Retornar todos os números até 100 que forem quadrados perfeitos. (1, 4, 9, …, 81, 100)
 function squareNumbers(){
@@ -51,6 +55,7 @@ function squareNumbers(){
     }
     return result;
 }
+console.log(squareNumbers());
 
 //Retornar os números contando de trás para frente de 20 até 1. (20, 19, 18, …, 2, 1)
 function countingBackwards(){
@@ -60,6 +65,7 @@ function countingBackwards(){
     }
     return result;
 }
+console.log(countingBackwards());
 
 //Retornar os números pares de 20 até 1. (20, 18, 16, …, 4, 2)
 function evenNumbersBackwards(){
@@ -71,6 +77,7 @@ function evenNumbersBackwards(){
     }
     return result;
 }
+console.log(evenNumbersBackwards());
 
 //Retornar os números ímpares de 20 até 1. (19, 17, 15, …, 3, 1)
 function oddNumbersBackwards(){
@@ -82,6 +89,7 @@ function oddNumbersBackwards(){
     }
     return result;
 }
+console.log(oddNumbersBackwards());
 
 //Retornar os múltiplos de 5 contando de trás para frente a partir de 100. (100, 95, 90, …, 10, 5)
 function multiplesOfFiveBackwards(){
@@ -93,6 +101,7 @@ function multiplesOfFiveBackwards(){
     }
     return result;
 }
+console.log(multiplesOfFiveBackwards());
 
 //Retornar os quadrados perfeitos contando de trás para frente a partir de 100. (100, 81, 64, …, 4, 1)
 function squareNumbersBackwards(){
@@ -105,3 +114,4 @@ function squareNumbersBackwards(){
     }
     return result;
 }
+console.log(squareNumbersBackwards());
